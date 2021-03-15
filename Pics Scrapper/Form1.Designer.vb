614d764e6b1237a2a6ddf0cdf0e65e2ffd8ddc12
Churrufli <churruflis@gmail.com>
2021-03-15 09:43:46 +0100

    Agregar archivos de proyecto.

---
+++ Pics Scrapper/Form1.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.url = New System.Windows.Forms.TextBox()
+        Me.scrap = New System.Windows.Forms.Button()
+        Me.txlog = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.dirname = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.patron = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.SuspendLayout
+        '
+        'url
+        '
+        Me.url.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.url.Location = New System.Drawing.Point(80, 21)
+        Me.url.Name = "url"
+        Me.url.Size = New System.Drawing.Size(844, 21)
+        Me.url.TabIndex = 0
+        '
+        'scrap
+        '
+        Me.scrap.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.scrap.Location = New System.Drawing.Point(231, 152)
+        Me.scrap.Name = "scrap"
+        Me.scrap.Size = New System.Drawing.Size(414, 27)
+        Me.scrap.TabIndex = 1
+        Me.scrap.Text = "Get images renamered"
+        Me.scrap.UseVisualStyleBackColor = true
+        '
+        'txlog
+        '
+        Me.txlog.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.txlog.Location = New System.Drawing.Point(34, 195)
+        Me.txlog.Multiline = true
+        Me.txlog.Name = "txlog"
+        Me.txlog.Size = New System.Drawing.Size(890, 197)
+        Me.txlog.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = true
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.Label1.Location = New System.Drawing.Point(37, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(32, 15)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "URL"
+        '
+        'dirname
+        '
+        Me.dirname.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.dirname.Location = New System.Drawing.Point(80, 77)
+        Me.dirname.Name = "dirname"
+        Me.dirname.Size = New System.Drawing.Size(100, 21)
+        Me.dirname.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = true
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.Label2.Location = New System.Drawing.Point(6, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(63, 15)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Set Folder"
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.Button2.Location = New System.Drawing.Point(760, 156)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(164, 23)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Delete Lands"
+        Me.Button2.UseVisualStyleBackColor = true
+        '
+        'patron
+        '
+        Me.patron.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.patron.Location = New System.Drawing.Point(302, 77)
+        Me.patron.Name = "patron"
+        Me.patron.Size = New System.Drawing.Size(100, 21)
+        Me.patron.TabIndex = 8
+        Me.patron.Text = ".png"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = true
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.Label3.Location = New System.Drawing.Point(214, 80)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(82, 15)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Type in server"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = true
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.Label4.Location = New System.Drawing.Point(77, 45)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(492, 15)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Example: https://magic.wizards.com/en/articles/archive/card-image-gallery/rivals-"& _ 
+    "of-ixalan"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = true
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.Label5.Location = New System.Drawing.Point(98, 101)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(82, 15)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Example: RIX"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = true
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.Label7.Location = New System.Drawing.Point(299, 101)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(116, 15)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Replace expression"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8!, 16!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(952, 417)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.patron)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.dirname)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txlog)
+        Me.Controls.Add(Me.scrap)
+        Me.Controls.Add(Me.url)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Download from Wizards Gallery Renaming Cards for Forge"
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+
+    Friend WithEvents url As TextBox
+    Friend WithEvents scrap As Button
+    Friend WithEvents txlog As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents dirname As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents patron As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label7 As Label
+End Class
---
+++ Pics Scrapper/Form1.vb
@@ -0,0 +1,237 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Text.RegularExpressions
+Imports HtmlAgilityPack
+
+Public Class Form1
+    Private Sub scrap_Click(sender As Object, e As EventArgs) Handles scrap.Click
+        'borro carpeta y creo
+        Dim mydir As New DirectoryInfo(dirname.Text)
+        If mydir.Exists = True Then
+            'System.IO.Directory.Delete(dirname.Text, True)
+        Else
+            mydir.Create()
+        End If
+
+
+        'primero tendría que leer la web y cojer las imagenes
+        Dim lawebes As String = readweb(url.Text)
+        Dim lawebin As String = readweb(Replace(url.Text, "/es/", "/en/"))
+
+        txlog.Text = extraer(lawebin, "<img", ">")
+        'Exit Sub
+
+        Dim lineas = Split(txlog.Text, vbCrLf)
+        For i = 0 To lineas.Length - 1
+            'If InStr(lineas(i).ToString, "png") > 0 Or InStr(lineas(i).ToString, "jpg") > 0 Then
+            Dim linea = lineas(i).ToString
+            'linea = Replace(linea, """", "")
+            If InStr(linea, patron.Text) > 0 Then
+
+                Dim imagen As String = extraer(linea, "src", patron.Text)
+
+                imagen = Replace(imagen, """", "")
+                imagen = Replace(imagen, "=", "")
+                ' ****lo comento para hacerlo internacional
+
+                'imagen = Replace(imagen, "/en_", "/sp_")
+                'imagen = Replace(imagen, "_en", "_es")
+                'imagen = Replace(imagen, "_EN_LR", "_ES")
+                'imagen = Replace(imagen, "_EN", "_ES")
+
+                ' ****
+
+                imagen = Replace(imagen, vbCrLf, Nothing)
+                imagen = imagen & patron.Text
+                'solo commander 16
+                'imagen = Replace(imagen, "_EN.png", "_SP.png")
+
+                Dim tit As String = extraer(linea, "alt=""", """")
+                tit = Replace(tit, """", "")
+                tit = Replace(tit, "=", "")
+                tit = RemoveWhitespace(tit)
+                tit = Replace(tit, vbCrLf, Nothing)
+                tit = Trim(tit)
+                txlog.AppendText("Saving " & tit & " from " & imagen & vbCrLf)
+                tit = Replace(tit, "&rsquo;", "'")
+                tit = Replace(tit, "///", "")
+                tit = Replace(tit, "//", "")
+                tit = Replace(tit, "/", "")
+                tit = Replace(tit, "&AElig;", "Ae")
+
+                Dim lands = "forest,plain,island,mountain,swamp,wastes"
+                Dim myland = Split(lands, ",")
+
+                For a = 0 To myland.Count - 1
+                    If InStr(LCase(tit), myland(a)) > 0 Then
+                        Dim myy = 1
+                        While File.Exists(dirname.Text & "/" & tit & myy & ".full.jpg") = True
+                            myy = myy + 1
+                            'If myy > 9 Then Exit While
+                        End While
+                        tit = tit & myy
+
+                        Exit For
+                    End If
+                Next a
+
+                If InStr(imagen, "/") = 0 Or IsNothing(tit) = False Then
+                    If Directory.Exists(dirname.Text) = False Then Directory.CreateDirectory(dirname.Text)
+                    If File.Exists(dirname.Text & "/" & tit & ".full.jpg") = False Then
+                        Dim hayerror = False
+                        Dim downloaded = False
+                        Try
+                            Dim Client As New WebClient
+                            Client.DownloadFile(imagen, dirname.Text & "/" & tit & ".full.jpg")
+                            txlog.AppendText(imagen & " " & tit & vbCrLf)
+                            downloaded = True
+                            'Client.Dispose()
+                        Catch
+                            hayerror = True
+                        End Try
+                        'comento esto para no bajar en inglés
+                        'If downloaded = False Then
+                        '    Try
+                        '        Dim Client As New WebClient
+                        '        Client.DownloadFile(Replace(imagen, "/sp_", "/en_"), dirname.Text & "/" & tit & ".full.jpg")
+                        '        txlog.AppendText(imagen & " " & tit & vbCrLf)
+                        '        downloaded = True
+                        '    Catch
+                        '        hayerror = True
+                        '    End Try
+                        ' End If
+
+                        If hayerror Then
+
+                            'txlog.AppendText(Err.Description & imagen & " " & tit)
+                            '    'Exit Sub
+                        End If
+
+
+                        txlog.AppendText(imagen & " " & tit & vbCrLf)
+                    End If
+                End If
+
+            End If
+
+        Next i
+        txlog.AppendText("END!")
+    End Sub
+
+    Function RemoveWhitespace(fs As String) As String
+        Try
+            fs = Regex.Replace(fs, "[ ]{2,}", " ")
+            fs = Regex.Replace(fs, "\s+", " ")
+        Catch
+        End Try
+
+        Return fs
+    End Function
+
+    Function normalizar(t) As String
+        normalizar = t
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'url.Text = "https://magic.wizards.com/es/articles/archive/card-image-gallery/hour-devastation"
+    End Sub
+
+    Public Function extraer(ByRef strSource As String, ByRef strStart As String, ByRef strEnd As String,
+                            Optional ByRef startPos As Integer = 0)
+
+        Dim iPos As Integer, iEnd As Integer, strResult As String, lenStart As Integer = strStart.Length
+        Dim res As String
+        Do Until iPos = - 1
+            strResult = String.Empty
+            iPos = strSource.IndexOf(strStart, startPos)
+            iEnd = strSource.IndexOf(strEnd, iPos + lenStart)
+            If iPos <> - 1 AndAlso iEnd <> - 1 Then
+                strResult = strSource.Substring(iPos + lenStart, iEnd - (iPos + lenStart))
+                res = res & strResult & vbCrLf
+                startPos = iPos + lenStart
+            End If
+        Loop
+        extraer = res
+    End Function
+
+
+    Function readweb(laUrl As String)
+        laUrl = Replace(laUrl, "'", "")
+        laUrl = Replace(laUrl, """", "")
+        Dim res As String
+        If laUrl = "" Then Exit Function
+
+        Dim request As WebRequest
+        Try
+            request = WebRequest.Create(laUrl)
+        Catch
+            'txlog.AppendText("Error reading" & laUrl & "  bad URL?")
+            readweb = ""
+            Exit Function
+        End Try
+
+
+        Dim response As WebResponse
+        Try
+            response = request.GetResponse()
+        Catch
+            readweb = ""
+            Exit Function
+        End Try
+        Dim reader As New StreamReader(response.GetResponseStream())
+        Try
+            res = reader.ReadToEnd()
+        Catch
+
+
+            Exit Function
+        End Try
+
+        reader.Close()
+        response.Close()
+        readweb = res
+    End Function
+
+    Function obtener(mainurl) As String
+        Dim doc As HtmlDocument
+        doc = New HtmlDocument()
+        Dim sourceString As String = New WebClient().DownloadString(mainurl)
+        doc.LoadHtml(sourceString)
+        Dim i = 0
+        Dim res = ""
+
+
+        For Each link As HtmlNode In doc.DocumentNode.SelectNodes("//img[@src]")
+            Dim linkAddress = GetAbsoluteUrl(link.Attributes("src").Value, mainurl)
+            Console.WriteLine("Image: {0}", linkAddress)
+            res = res & linkAddress.ToString & ","
+        Next
+        obtener = res
+    End Function
+
+    Function GetAbsoluteUrl(partialUrl As String, baseUrl As String)
+        Dim myUri = New Uri(partialUrl, UriKind.RelativeOrAbsolute)
+        If (myUri.IsAbsoluteUri = False) Then
+            myUri = New Uri(New Uri(baseUrl), partialUrl)
+        End If
+        GetAbsoluteUrl = myUri
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim mydir As New DirectoryInfo(dirname.Text)
+        For Each fi In mydir.GetFiles
+            Dim n As String = LCase(fi.Name)
+            Dim borrar = False
+
+            If InStr(n, "plains") > 0 Then File.Delete(dirname.Text & "/" & fi.Name)
+            If InStr(n, "forest") > 0 Then File.Delete(dirname.Text & "/" & fi.Name)
+            If InStr(n, "island") > 0 Then File.Delete(dirname.Text & "/" & fi.Name)
+            If InStr(n, "swamp") > 0 Then File.Delete(dirname.Text & "/" & fi.Name)
+            If InStr(n, "mountain") > 0 Then File.Delete(dirname.Text & "/" & fi.Name)
+            If InStr(n, "wastes") > 0 Then File.Delete(dirname.Text & "/" & fi.Name)
+        Next
+    End Sub
+
+
+    '
+End Class
